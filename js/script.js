3145573bc01bc16bd6986a893fc2fdc1c91192c3
Yash raj <124022190+Yaahrit@users.noreply.github.com>
2024-01-30 12:28:03 +0530

    adding something new

--- js/script.js
+++ js/script.js
@@ -2,17 +2,24 @@
 function avneet() {
   // Your desired functionality goes here
   console.log("avneet function is called");
+
+  // Toggle the visibility of the chat box
+  toggleChatBoxVisibility();
 }
 
 // Your existing JavaScript code
-let back = document.querySelector(".back");
-let chatBox = document.querySelector(".chatBox");
-let open = document.querySelector(".open");
+document.addEventListener("DOMContentLoaded", function () {
+  let back = document.querySelector(".back");
 
-back.onclick = () => {
-  chatBox.classList.add("hide");
-};
+  back.addEventListener("click", function () {
+    // Toggle the visibility of the chat box
+    toggleChatBoxVisibility();
+  });
+});
 
-open.onclick = function () {
-  chatBox.classList.remove("hide");
-};
+// Function to toggle the visibility of the chat box
+function toggleChatBoxVisibility() {
+  let chatBox = document.querySelector(".chatBox");
+  chatBox.classList.toggle("hide");
+  // Additional logic can be added here for any specific functionality when toggling
+}
